12846282ffead6aa3b938806067896087aec2f1d
fiona <fi.carty@gmail.com>
2016-12-16 23:40:20 -0800

    testing beacons in range & also filtering out unknown beacons

--- estimoteStarter/ViewController.swift
+++ estimoteStarter/ViewController.swift
@@ -15,14 +15,22 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self
+        let region = CLBeaconRegion(proximityUUID: NSUUID(uuidString: "B9407F30-F5F8-466E-AFF9-25556B57FE6D") as! UUID, identifier: "Estimotes")
+        // MARK: if the request hasn't been authorized
+        if (CLLocationManager.authorizationStatus() != CLAuthorizationStatus.authorizedWhenInUse) {
+            locationManager.requestWhenInUseAuthorization()
+        }
+        locationManager.startRangingBeacons(in: region)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
+        print(beacons)
+        // MARK: new array that filters out all the unknown beacons
+        let knownBeacons = beacons.filter{ $0.proximity != CLProximity.unknown }
     }
-
 
 }
 
